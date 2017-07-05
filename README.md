323a467d38f0c461cbe7e09fec2c2a8f63104033
James Jervis <jjervis@relivinc.com>
2017-07-05 16:27:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+RCMS
+====
+
+Version 2.0 of RCM Core
---
+++ src/ModuleConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Rcms;
+
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+class ModuleConfig
+{
+    /**
+     * __invoke
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        return [
+
+        ];
+    }
+}
