7ec10a6f43605ff235b2f29b7a61af35b2dc71d5
James Jervis <jjervis@relivinc.com>
2017-07-11 11:17:00 -0500

    Update

---
+++ core/Cache/Service/Cache.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zrcms\Core\Cache\Service;
+
+use Zend\Cache\Storage\StorageInterface;
+
+/**
+ * @GammaRelease
+ * Interface Cache
+ *
+ * @author    James Jervis
+ * @license   License.txt
+ * @link      https://github.com/jerv13
+ */
+interface Cache extends StorageInterface
+{
+
+}
