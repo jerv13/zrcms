39d71f5c22c6c1a0dca38631039d7081ebee290e
James Jervis <jjervis@relivinc.com>
2017-07-14 18:03:09 -0500

    Update

--- core/Container/Api/Repository/FindContainerPathsByHtmlMustache.php
+++ core/Container/Api/Repository/FindContainerPathsByHtmlMustache.php
@@ -2,6 +2,8 @@
 
 namespace Zrcms\Core\Container\Api\Repository;
 
+use Zrcms\Core\Container\Model\ContainerProperties;
+
 /**
  * @author James Jervis - https://github.com/jerv13
  */
@@ -9,13 +11,17 @@ class FindContainerPathsByHtmlMustache implements FindContainerPathsByHtml
 {
     /**
      * @param string $html
-     * @param array $options
+     * @param array  $options
      *
      * @return array ['{container-name}']
      */
     public function __invoke(string $html, array $options = [])
     {
-        preg_match_all('/\{\{container.([^}:]+)\}\}/', $html, $matches);
+        preg_match_all(
+            '/\{\{' . ContainerProperties::RENDER_NAMESPACE . '.([^}:]+)\}\}/',
+            $html,
+            $matches
+        );
 
         return $matches[1];
     }
