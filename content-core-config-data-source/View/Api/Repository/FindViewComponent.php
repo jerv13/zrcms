fef24f6ef01aa46c955bc89d772330d7d50cda96
James Jervis <jjervis@relivinc.com>
2017-07-24 10:57:55 -0500

    Update

---
+++ content-core-config-data-source/View/Api/Repository/FindViewComponent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zrcms\ContentCoreConfigDataSource\View\Api\Repository;
+
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+class FindViewComponent
+{
+
+}
---
+++ content-core/View/Model/ViewComponentBasic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zrcms\ContentCore\View\Model;
+
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+class ViewComponentBasic extends ViewComponentAbstract implements ViewComponent
+{
+
+}
