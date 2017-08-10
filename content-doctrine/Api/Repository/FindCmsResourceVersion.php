f8d196d616e3c3f3615e217daf12beae14c72f37
James Jervis <jjervis@relivinc.com>
2017-08-10 14:49:35 -0500

    Update

--- content-doctrine/Api/Repository/FindCmsResourceVersion.php
+++ content-doctrine/Api/Repository/FindCmsResourceVersion.php
@@ -3,11 +3,19 @@
 namespace Zrcms\ContentDoctrine\Api\Repository;
 
 use Doctrine\ORM\EntityManager;
+use Zrcms\Content\Model\CmsResource;
+use Zrcms\Content\Model\Content;
+use Zrcms\Content\Model\ContentVersion;
+use Zrcms\Content\Model\PropertiesCmsResource;
+use Zrcms\ContentDoctrine\Api\ApiAbstract;
+use Zrcms\ContentDoctrine\Api\BasicCmsResourceTrait;
 
 /**
  * @author James Jervis - https://github.com/jerv13
  */
 class FindCmsResourceVersion
+    extends ApiAbstract
+    implements \Zrcms\Content\Api\Repository\FindCmsResourceVersion
 {
     use BasicCmsResourceTrait;
 
@@ -24,17 +32,17 @@ class FindCmsResourceVersion
     /**
      * @var string
      */
-    protected $entityClassCmsResourcePublishHistory;
-
-    /**
-     * @var string
-     */
     protected $entityClassContentVersion;
 
     /**
      * @var string
      */
     protected $classCmsResourceBasic;
+
+    /**
+     * @var string
+     */
+    protected $classContentVersion;
 
     /**
      * @param EntityManager $entityManager
@@ -56,19 +64,48 @@ class FindCmsResourceVersion
         );
 
         $this->assertValidEntityClass(
-            $entityClassCmsResourcePublishHistory,
-            CmsResourcePublishHistory::class
-        );
-
-        $this->assertValidEntityClass(
             $entityClassContentVersion,
             ContentVersion::class
         );
 
         $this->entityManager = $entityManager;
-        $this->entityClassCmsResourcePublishHistory = $entityClassCmsResourcePublishHistory;
         $this->entityClassCmsResource = $entityClassCmsResource;
         $this->entityClassContentVersion = $entityClassContentVersion;
         $this->classCmsResourceBasic = $classCmsResourceBasic;
+        $this->classContentVersion = $classContentVersion;
+    }
+
+    /**
+     * @param string $cmsResourceId
+     * @param array  $options
+     *
+     * @return void
+     */
+    public function __invoke(
+        string $cmsResourceId,
+        array $options = []
+    ) {
+        $cmsResourceIdName = PropertiesCmsResource::ID;
+
+        $queryParams = [
+            $cmsResourceId=> 'cmsResourceId'
+        ];
+
+        // @todo Add prepared statements not concat
+        $query = ""
+            . "SELECT resource FROM {$this->entityClassCmsResource} resource"
+            . " WHERE resource.{$cmsResourceIdName} = :cmsResourceId"
+            . " JOIN {$this->entityClassContentVersion} version WITH resource.contentVersionId =  version.id";
+
+
+        $dQuery = $this->entityManager->createQuery($query);
+
+        foreach ($queryParams as $value => $queryParam) {
+            $dQuery->setParameter($queryParam, $value);
+        }
+
+        $cmsResourceVersion = $dQuery->getFirstResult();
+
+        ddd($cmsResourceVersion);
     }
 }
