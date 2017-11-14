02f620b370a85b9dd6e4065fd34569e99c0e354c
James Jervis <jjervis@relivinc.com>
2017-11-14 11:45:13 -0600

    Notes for install

--- install/modifiy/0.0.0-install.php
+++ install/modifiy/0.0.0-install.php
@@ -2,6 +2,24 @@
 /**
  * 0.0.0-install.php
  */
-return function ($serviceContainer) {
+return function (\Psr\Container\ContainerInterface $serviceContainer) {
     // Run scripts here
+
+    // @todo EXAMPLE ONLY - make this work correctly
+    /** @var \Doctrine\ORM\EntityManager $em */
+    //$em = $serviceContainer->get(\Doctrine\ORM\EntityManager::class);
+    //
+    //$metadatas = $em->getMetadataFactory()->getAllMetadata();
+    //
+    //$schemeTool = new \Doctrine\ORM\Tools\SchemaTool(
+    //    $em
+    //);
+    //
+    //$createSql = $schemeTool->getCreateSchemaSql(
+    //    $metadatas
+    //);
+    //
+    //ddd(
+    //    $createSql
+    //);
 };
