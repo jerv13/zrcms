41391b120f850ff79bd080b80d37c99e70637134
James Jervis <jjervis@relivinc.com>
2017-11-14 10:29:11 -0600

    Notes for install

--- content-doctrine/ModuleConfig.php
+++ content-doctrine/ModuleConfig.php
@@ -14,6 +14,33 @@ class ModuleConfig
      */
     public function __invoke()
     {
-        return [];
+        return [
+            'doctrine' => [
+                /** @todo
+                'configuration' => [
+                    'orm_default' => [
+                        'metadata_cache' => 'doctrine_cache',
+                        'query_cache' => 'doctrine_cache',
+                        'result_cache' => 'doctrine_cache',
+                        'hydration_cache' => 'doctrine_cache',
+                    ],
+                    'cache' => [
+                    ],
+                ],
+                'connection' => [
+                    'orm_default' => [
+                        'driverClass' => \Doctrine\DBAL\Driver\PDOMySql\Driver::class,
+                        'params' => [
+                            'host' => \Reliv\Secrets\Secrets::getValue('db_host'),
+                            'port' => \Reliv\Secrets\Secrets::getValue('db_port'),
+                            'user' => \Reliv\Secrets\Secrets::getValue('db_user'),
+                            'password' => \Reliv\Secrets\Secrets::getValue('db_pass'),
+                            'dbname' => \Reliv\Secrets\Secrets::getValue('db_name'),
+                        ],
+                    ],
+                ],
+                 **/
+            ],
+        ];
     }
 }
--- install/README.md
+++ install/README.md
@@ -1,9 +1,29 @@
 Install
 =======
 
+Install Application
+-------------------
+
 @todo Create scripts to build server and setup code
 
 - Install skeleton with core dependencies
-- Add config for DB connection (doctrine) config
+- Add config for DB connection (doctrine) config (secrets)
 - Test connections
 - Import some default data (or import file) to create sites and pages
+- Multi-server syncing: 
+    - might use GIT for this
+    - might use SFTP or FTP
+
+Install Component
+-----------------
+
+@todo Create way to install components
+
+- Composer might be good solution
+- Need to be able to track version
+- Need to be able to make DB changes
+    - Install
+    - Upgrade
+    - Downgrade
+    - Delete
+- Need multi-server syncing
