3d01b279b3074794f8453c5fcce8b9e149e61953
James Jervis <jjervis@relivinc.com>
2018-03-31 02:12:44 -0500

    Update

---
+++ core-block/src/Api/Render/RenderBlockMissingDiv.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Zrcms\CoreBlock\Api\Render;
+
+use Reliv\ArrayProperties\Property;
+use Zrcms\Core\Model\Content;
+use Zrcms\CoreBlock\Model\Block;
+
+/**
+ * Component is missing or has been removed, use this renderer
+ *
+ * @author James Jervis - https://github.com/jerv13
+ */
+class RenderBlockMissingDiv implements RenderBlockMissing
+{
+    /**
+     * @param Block|Content $block
+     * @param array         $renderTags ['render-tag' => '{html}']
+     * @param array         $options
+     *
+     * @return string
+     */
+    public function __invoke(
+        Content $block,
+        array $renderTags,
+        array $options = []
+    ): string {
+        $reason = Property::getString(
+            $options,
+            self::OPTION_REASON,
+            self::DEFAULT_REASON
+        );
+
+        $message = 'BLOCK COMPONENT (' . $block->getBlockComponentName() . ')'
+            . ' NOT RENDERED (' . $reason . ')  '
+            . ' for block: ' . $block->getId();
+
+        return "\n"
+            . '<div class="block-not-rendered" style="display: none">' . $message . '</div>'
+            . "\n";
+    }
+}
