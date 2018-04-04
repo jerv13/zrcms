8d37edc85411736b8169023d267f203d5cc4fd97
James Jervis <jjervis@relivinc.com>
2018-04-04 13:46:56 -0500

    Split import

--- importer/src/Api/ImportFactory.php
+++ importer/src/Api/ImportFactory.php
@@ -3,17 +3,6 @@
 namespace Zrcms\Importer\Api;
 
 use Psr\Container\ContainerInterface;
-use Zrcms\CorePage\Api\CmsResource\CreatePageCmsResource;
-use Zrcms\CorePage\Api\CmsResource\CreatePageTemplateCmsResource;
-use Zrcms\CorePage\Api\Content\InsertPageVersion;
-use Zrcms\CoreRedirect\Api\CmsResource\CreateRedirectCmsResource;
-use Zrcms\CoreRedirect\Api\CmsResource\FindRedirectCmsResource;
-use Zrcms\CoreRedirect\Api\Content\InsertRedirectVersion;
-use Zrcms\CoreSite\Api\CmsResource\CreateSiteCmsResource;
-use Zrcms\CoreSite\Api\CmsResource\FindSiteCmsResource;
-use Zrcms\CoreSite\Api\Content\InsertSiteVersion;
-use Zrcms\CoreSiteContainer\Api\CmsResource\CreateSiteContainerCmsResource;
-use Zrcms\CoreSiteContainer\Api\Content\InsertSiteContainerVersion;
 
 /**
  * @author James Jervis - https://github.com/jerv13
